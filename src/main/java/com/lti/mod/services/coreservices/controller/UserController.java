8de9b78c0940eb357bdac98c97ca55817ed6e9b5
dhoRam <67107790+dhoRam@users.noreply.github.com>
2020-08-06 06:27:23 +0530

    dummy login service

--- src/main/java/com/lti/mod/services/coreservices/controller/UserController.java
+++ src/main/java/com/lti/mod/services/coreservices/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.lti.mod.services.coreservices.controller;
 
+import com.lti.mod.services.coreservices.model.UserDao;
 import com.lti.mod.services.coreservices.model.UserDto;
+import com.lti.mod.services.coreservices.repository.UserRepository;
 import com.lti.mod.services.coreservices.service.UserRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,11 +14,18 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     private UserRegistrationService userDetailsService;
-
+    @Autowired
+    private UserRepository userDao;
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ResponseEntity<?> saveUser(@RequestBody UserDto user) throws Exception {
         System.out.println("Saved ##########");
         return ResponseEntity.ok(userDetailsService.save(user));
     }
 
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> authenticate(@RequestBody UserDto user) throws Exception {
+        System.out.println(user.getEmail());
+        UserDao usr = userDao.findByEmail(user.getEmail());
+        return new ResponseEntity<>(usr , HttpStatus.OK);
+    }
 }
